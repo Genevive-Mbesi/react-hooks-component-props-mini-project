7a767461c490a4bc99d03d560e0f627fde5da67f
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-07-21 18:45:04 +0300

    assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Article from "./Article";
+
+export default function ArticleList({posts}){
+    console.log(posts)
+    const articlesElement = posts.map((post) => {
+        return (<Article
+            key={post.id} 
+            title={post.title}
+            date={post.date}
+            preview={post.preview}/>)
+    })
+    return (
+    <main>{articlesElement}</main>
+    )
+}
